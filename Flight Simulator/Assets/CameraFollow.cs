9da53177e9e53aa82d74e10805ea235e533b4331
Nick <nrs003@ucsd.edu>
2020-10-17 16:05:08 -0700

    First commit of Assets folder

---
+++ Flight Simulator/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform objPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Object's position: " + objPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Flight Simulator/Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Variables
+    public float pitch_rate = 20.0f;
+    public float yaw_rate = 10.0f;
+    public float roll_rate = 20.0f;
+    public float thrust = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Time.deltaTime converts to m/s instead of m/frame
+        float pitch = Input.GetAxis("Vertical") * pitch_rate * Time.deltaTime;
+        float roll = Input.GetAxis("Horizontal") * roll_rate * Time.deltaTime;
+
+        transform.Rotate(pitch, 0, 0);
+        transform.Rotate(0, 0, -roll);
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * thrust * Time.deltaTime);
+        }
+    }
+}
